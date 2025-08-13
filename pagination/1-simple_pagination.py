bfdecf15cd9c5f70f74ac631f8e34ff132a4c0ac
georges479 <g.ismael071221@gmail.com>
2025-08-13 13:55:30 +0200

    correction task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -36,8 +36,8 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Get a page of dataset"""
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
         start, end = index_range(page, page_size)
 
         data = self.dataset()
