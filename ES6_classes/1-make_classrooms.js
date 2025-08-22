d3e74ae7a64fef5632da34aed95213c18325b728
georges479 <g.ismael071221@gmail.com>
2025-08-22 17:19:51 +0200

    task 0 and 1 test

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom.js';
+
+
+export default function initializeRooms() {
+	const newClassRooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+
+	return newClassRooms;
+}
