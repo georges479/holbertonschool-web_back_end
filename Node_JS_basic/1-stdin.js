ce8ae6818287381c3c97637e0ea11c078430fc8b
georges479 <g.ismael071221@gmail.com>
2025-09-06 13:05:33 +0200

    correction task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,23 +1,8 @@
-const readline = require('readline');
-if (process.stdin.isTTY) {
-	// Interactive mode
-	const rlname = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	});
-
-rlname.question("Welcome to Holberton School, what is your name?\n", (name) => {
-	console.log(`Your name is: ${name}`);
-	rlname.close();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
-} else {
-	// Redirection mode (pipe or file redirection)
-	console.log("Welcome to Holberton School, what is your name?");
-	process.stdin.on("data", (data) => {
-		const name = data.toString().trim();
-		console.log(`Your name is: ${name}`);
-		process.stdin.on("end", () => {
-			console.log("This important software is now closing");
-		});
-	});
-}
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
