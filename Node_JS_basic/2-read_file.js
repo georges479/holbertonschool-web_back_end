39aeeb4e13e2ca78e674727b4f4543f427bb67d6
georges479 <g.ismael071221@gmail.com>
2025-09-08 11:11:10 +0200

    correction 2 finished

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,35 +1,34 @@
 const fs = require('fs');
 
-
 function countStudents(filePath) {
-	let content;
+  let content;
 
-	try {
-		content = fs.readFileSync(filePath, 'utf-8'); // Read the file content in synchronisation
-	} catch (error) {
-		throw new Error('Cannot load the database');
-	}
+  try {
+    content = fs.readFileSync(filePath, 'utf-8'); // Read the file content in synchronisation
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 
-	const lines = content.split('\n'); // Slice in lines the string of content.
-	const cleanLines = lines.filter((lines) => lines.trim() !== ''); // Delete de emptys lines of end of file.
-	const students = cleanLines.slice(1); // Delete the header line.
-	console.log(`Number of students: ${students.length}`);
+  const lines = content.split('\n'); // Slice in lines the string of content.
+  const cleanLines = lines.filter((lines) => lines.trim() !== ''); // Delete de emptys lines of end of file.
+  const students = cleanLines.slice(1); // Delete the header line.
+  console.log(`Number of students: ${students.length}`);
 
-	const studentsByField = {}; // Object to store the students by field.
+  const studentsByField = {}; // Object to store the students by field.
 
-	for (const line of students) { // Iterate over each student line.
-		const colums = line.split(','); // Split each line by comma to get the columns.
-		const firstName = colums[0];
-		const field = colums[3];
+  for (const line of students) { // Iterate over each student line.
+    const colums = line.split(','); // Split each line by comma to get the columns.
+    const firstName = colums[0];
+    const field = colums[3];
 
-		if (!studentsByField[field]) { // If the field does not exist, create an array for it.
-			studentsByField[field] = [];
-		}
-		studentsByField[field].push(firstName); // Add the first name to the corresponding field array.
-	}
-	for (const field in studentsByField) { // Iterate over each field in the object.
-		console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
-	}
+    if (!studentsByField[field]) { // If the field does not exist, create an array for it.
+      studentsByField[field] = [];
+    }
+    studentsByField[field].push(firstName); // Add the first name to the corresponding field array.
+  }
+  for (const field in studentsByField) { // Iterate over each field in the object.
+    console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+  }
 }
 
 module.exports = countStudents;
