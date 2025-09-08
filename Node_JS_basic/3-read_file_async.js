c2ccc0da2a7f833268346c2b3a74dae06836bc0d
georges479 <g.ismael071221@gmail.com>
2025-09-08 17:23:35 +0200

    task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises;
+
+function countStudents(filePath) {
+  return fs.readFile(filePath, "utf-8")
+  .then((content) => {
+    const lines = content.split('\n');
+    const cleanLines = lines.filter((line) => lines.trim() !== '');
+    const students = cleanLines.slice(1);
+    console.log(`Number of students: ${students.length}`);
+
+    const studentByField = {};
+
+    for (const line of students) {
+      const colums = line.split(',');
+      const firstName = colums[0];
+      const field = colums[3];
+
+      if (!studentByField[field]) {
+        studentByField[field] = [];
+      }
+      studentByField[field].push(firstName);
+    }
+    for (const field of Object.keys(studentByField)) {
+      console.log(`Number of students in ${field}: ${studentByField[field].length}. List: ${studentByField[field].join(', ')}`);
+    }
+  }) .catch(() => {
+    throw new Error('Cannot load the database');
+  });
+}
+module.exports = countStudents;
