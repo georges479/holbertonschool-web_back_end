f0e3782a8efcef5a4606b7dbf86bfcea1adea270
georges479 <g.ismael071221@gmail.com>
2025-09-11 15:57:52 +0200

    task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -10,29 +10,29 @@ const app = http.createServer((req, res) => {
     const dbFile = process.argv[2];
 
     fs.promises.readFile(dbFile, 'utf-8')
-    .then((content) => {
-      const lines = content.split('\n').filter(line => line.trim() !== '');
-      const students = lines.slice(1);
-      let output = `Number of students: ${students.length}\n`;
+      .then((content) => {
+        const lines = content.split('\n').filter(line => line.trim() !== '');
+        const students = lines.slice(1);
+        let output = `Number of students: ${students.length}\n`;
 
-      const studentsByField = {};
-      for (const line of students) {
-        const cols = line.split(',');
-        const firstName = cols[0];
-        const field = cols[3];
-        if (!studentsByField[field]) studentsByField[field] = [];
-        studentsByField[field].push(firstName);
-      }
+        const studentsByField = {};
+        for (const line of students) {
+          const cols = line.split(',');
+          const firstName = cols[0];
+          const field = cols[3];
+          if (!studentsByField[field]) studentsByField[field] = [];
+          studentsByField[field].push(firstName);
+        }
 
-      for (const field of Object.keys(studentsByField)) {
-        output += `Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}\n`;
-      }
+        for (const field of Object.keys(studentsByField)) {
+          output += `Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}\n`;
+        }
 
-      res.end('This is the list of our students\n' + output.trim());
-    })
-    .catch(() => {
-      res.end('This is the list of our students\nCannot load the database');
-    });
+        res.end('This is the list of our students\n' + output.trim());
+      })
+      .catch(() => {
+        res.end('This is the list of our students\nCannot load the database');
+      });
   }
 });
 app.listen(1245);
