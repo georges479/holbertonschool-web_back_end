8847e479dcbae3eead8c664642754d1273aeb92b
georges479 <g.ismael071221@gmail.com>
2025-09-11 15:33:29 +0200

    task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,5 +1,5 @@
-const countStudents = require('./3-read_file_async');
 const http = require('http');
+const fs = require('fs');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -8,10 +8,29 @@ const app = http.createServer((req, res) => {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     const dbFile = process.argv[2];
-    countStudents(dbFile)
-    .then((output) => {
-      res.end('This is the list of our students\n' + output);
-    }).catch(() => {
+
+    fs.promises.readFile(dbFile, 'utf-8')
+    .then((content) => {
+      const lines = content.split('\n').filter(line => line.trim() !== '');
+      const students = lines.slice(1);
+      let output = `Number of students: ${students.length}\n`;
+
+      const studentsByField = {};
+      for (const line of students) {
+        const cols = line.split(',');
+        const firstName = cols[0];
+        const field = cols[3];
+        if (!studentsByField[field]) studentsByField[field] = [];
+        studentsByField[field].push(firstName);
+      }
+
+      for (const field of Object.keys(studentsByField)) {
+        output += `Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}\n`;
+      }
+
+      res.end('This is the list of our students\n' + output.trim());
+    })
+    .catch(() => {
       res.end('This is the list of our students\nCannot load the database');
     });
   }
