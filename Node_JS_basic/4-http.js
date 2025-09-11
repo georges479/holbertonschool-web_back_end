3c7e395edd4038b2b61011de139be30405ec5bea
georges479 <g.ismael071221@gmail.com>
2025-09-11 14:14:24 +0200

    task 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-	res.writeHead(200, { string: 'text/plain' });
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
 	res.end('Hello Holberton School!');
 });
 app.listen(1245);
 
-module.export = app;
+module.exports = app;
