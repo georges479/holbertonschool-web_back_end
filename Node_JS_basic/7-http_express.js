f0486bcacae154fcb06ada830b7eeab9880831ae
georges479 <g.ismael071221@gmail.com>
2025-11-26 13:02:16 +0100

    test

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,44 +1,28 @@
-const fs = require('fs').promises;
+const express = require('express');
+const dataFilePath = process.argv[2];
+const countStudents = require('./3-read_file_async.js');
 
-function countStudents(filePath) {
-  return fs.readFile(filePath, 'utf-8')
-    .then((content) => {
-      const lines = content.split('\n');
-      const cleanLines = lines.filter((line) => line.trim() !== '');
-      const students = cleanLines.slice(1);
+const app = express();
 
-      console.log(`Number of students: ${students.length}`);
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
 
-      const studentByField = {};
+app.get('/students', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
 
-      for (const line of students) {
-        const colums = line.split(',');
-        const firstName = colums[0];
-        const field = colums[3];
-
-        if (!studentByField[field]) {
-          studentByField[field] = [];
-        }
-        studentByField[field].push(firstName);
-      }
-
-      let output = `Number of students: ${students.length}`;
-
-      for (const field of Object.keys(studentByField)) {
-        const list = studentByField[field].join(', ');
-        const count = studentByField[field].length;
-
-        console.log(`Number of students in ${field}: ${count}. List: ${list}`);
-
-        output += `\nNumber of students in ${field}: ${count}. List: ${list}`;
-      }
-
-
-      return output;
+  countStudents(dataFilePath)
+    .then((msg) => {
+      res.send(`This is the list of our students\n${msg}`);
     })
     .catch(() => {
-      throw new Error('Cannot load the database');
+      res.send('This is the list of our students\nCannot load the database');
     });
-}
+});
 
-module.exports = countStudents;
+app.listen(1245, () => {
+  console.log('Server is listening on http://localhost:1245');
+});
+
+module.exports = app;
